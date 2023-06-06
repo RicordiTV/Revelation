61d81c3a0ffa01f842cc49da25002d4313ec51e6
Francesco96tu <69601501+RicordiTV@users.noreply.github.com>
2023-06-07 00:55:47 +0200

    .

---
+++ Versione/QRRevelation/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace QRRevelation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.txtQRCode = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(334, 6);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camera:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cboDevice
+            // 
+            this.cboDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDevice.FormattingEnabled = true;
+            this.cboDevice.Location = new System.Drawing.Point(76, 6);
+            this.cboDevice.Name = "cboDevice";
+            this.cboDevice.Size = new System.Drawing.Size(252, 24);
+            this.cboDevice.TabIndex = 2;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Black;
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(5, 35);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1331, 590);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // txtQRCode
+            // 
+            this.txtQRCode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.txtQRCode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtQRCode.Font = new System.Drawing.Font("Comic Sans MS", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQRCode.ForeColor = System.Drawing.Color.White;
+            this.txtQRCode.Location = new System.Drawing.Point(5, 632);
+            this.txtQRCode.Name = "txtQRCode";
+            this.txtQRCode.Size = new System.Drawing.Size(888, 70);
+            this.txtQRCode.TabIndex = 4;
+            this.txtQRCode.TextChanged += new System.EventHandler(this.txtQRCode_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.txtQRCode);
+            this.Controls.Add(this.cboDevice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "QRRevelation";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboDevice;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox txtQRCode;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Versione/QRRevelation/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+using ZXing.Presentation;
+using BarcodeReader = ZXing.BarcodeReader;
+
+namespace QRRevelation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection FilterInfoCollection;
+        VideoCaptureDevice captureDevice;
+        private void label1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtQRCode.Text = "";
+            captureDevice = new VideoCaptureDevice(FilterInfoCollection[cboDevice.SelectedIndex].MonikerString);
+            captureDevice.NewFrame += CaptureDevice_NewFrame;
+            captureDevice.Start();
+            timer1.Start();
+        }
+
+        private void CaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FilterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in FilterInfoCollection)
+                cboDevice.Items.Add(filterInfo.Name);
+            cboDevice.SelectedIndex = 0;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+          
+            if (captureDevice.IsRunning)
+                captureDevice.Stop();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(pictureBox.Image != null)
+            {
+                BarcodeReader barcodeReader = new BarcodeReader();
+                Result result = barcodeReader.Decode((Bitmap)pictureBox.Image);
+                    if (result !=null)
+                {
+                  
+                    txtQRCode.Text = result.ToString();
+                    timer1.Stop();
+                
+                    if (captureDevice.IsRunning)
+                        captureDevice.Stop();
+
+                 
+
+                }
+                
+            }
+
+        }
+
+        private void txtQRCode_TextChanged(object sender, EventArgs e)
+        {
+            if (txtQRCode.Text== "Solitudine") {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Solitudione.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Sorpresa")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Sorpresa.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Appagamento")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Appagamento.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Soddisfazione")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Soddisfazione.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Sensazione di colpa")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Sensazione di colpa.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Rimorso")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Rimorso.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Rabbia")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Rabbia.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Paura")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Paura.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Orgoglio")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Orgoglio.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Nostalgia")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Nostalgia.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Invidia")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Invidia.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Inquietudine")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Inquietudine.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Felicità")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Felicità.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Euforia")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Euforia.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Disgusto")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Disgusto.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Ansia")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Ansia.wav");
+                player.Play();
+            }
+            if (txtQRCode.Text == "Delusione")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Delusione.wav");
+           
+                player.Play();
+            }
+            if (txtQRCode.Text == "Tristezza")
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"Tristezza.wav");
+                player.Play();
+            }
+            
+
+        }
+    }
+}
